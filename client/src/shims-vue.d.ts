e1a0157f5a10827997141a98d9a9283f2ccde344
yuri <yuri@yurideMacBook-Pro.local>
2018-06-30 21:09:42 +0800

    更新配置

--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -3,12 +3,12 @@ declare module '*.vue' {
   export default Vue;
 }
 declare module 'vue/types/vue' {
-    interface Vue {
-      $toast: any;
-    }
-    interface VueConstructor {
-      toast: any
-    }
+  interface Vue {
+    $toast: any;
+  }
+  interface VueConstructor {
+    toast: any
+  }
 }
 
 declare var $api:any
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   lintOnSave: false,
-  compiler: true,
+  runtimeCompiler: true,
   devServer: {
     proxy: {
       '.*': {
